360fb8d19a786c2826fe90e76334093fee34a488
Fatih <fatihsen539@gmail.com>
2022-02-28 23:45:24 -0800

    Create hanoi.go

---
+++ Towers of Hanoi/hanoi.go
@@ -0,0 +1,20 @@
+package main
+
+func main(){
+	Towers(4, "Pike 1", "Pike 2", "Pike 3")
+}
+var step int = 0
+func print_move(fr string, to string) {
+	step++
+	println("Step", step, ": move from ", fr, " to ", to)
+}
+func Towers(n int, fr string, to string, spare string) {
+	if n == 1 {
+		print_move(fr, to)
+	} else {
+		Towers(n-1, fr, spare, to)
+		Towers(1, fr, to, spare)
+		Towers(n-1, spare, to, fr)
+	}
+
+}
